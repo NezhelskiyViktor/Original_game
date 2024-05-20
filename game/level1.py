ddcf22cab730a63a33c57ce1a672a7da6c5f831a
ergwat <denis.anisimov@gmail.com>
2024-05-20 17:25:52 +0300

    уже прыгает и бегает

--- game/level1.py
+++ game/level1.py
@@ -4,7 +4,8 @@ import resources
 import physics as ph
 
 
-def create_grass(x, y, length):
+# функция для создания любых, но ОДИНАКОВЫХ платформ
+def create_grass(x, y, length): # вводные данные - стартовые координаты (х,у) и длина
     platform_end = x+length
     while x < platform_end:
         grass = pg.sprite.Sprite()
@@ -12,7 +13,8 @@ def create_grass(x, y, length):
         grass.rect = grass.image.get_rect()
         grass.rect.y = y - grass.rect.height  # Позиционируем у нижнего края окна
         grass.rect.x = x
-        all_sprites.add(grass)
+        platforms.add(grass)
+        print(platforms)
         x += grass.rect.width  # Перемещаем X на ширину спрайта для следующего спрайта
 
 
@@ -23,19 +25,27 @@ screen = pg.display.set_mode((level1.screen_width, level1.screen_height))
 pg.display.set_caption(level1.caption)
 clock = pg.time.Clock()
 
-# Создаю группу спрайтов, чтобы потом обновлять все разом
-all_sprites = pg.sprite.Group()
+# Создаю 2 группы спрайтов (пока пустые), чтобы потом обновлять все разом
+all_sprites = pg.sprite.Group() # все остальные спрайты
+platforms = pg.sprite.Group() # платформы - отдельно для проверки коллизии (для прыжков и падений)
+
+# создаю фон
+bg = pg.sprite.Sprite()
+bg.image = pg.image.load('../res/graphics/fon_03.jpg')
+bg.rect = bg.image.get_rect()
+bg.rect.y = 0
+bg.rect.x = 0
+all_sprites.add(bg)
 
 # создаю землю и платформы
 create_grass(0, level1.screen_height,level1.screen_width) # ЗЕМЛЯ
-create_grass(200, 550, 300) # ПЛАТФОРМА
-create_grass(600, 350, 300) # ПЛАТФОРМА
+create_grass(200, 620, 300) # ПЛАТФОРМА 1
+create_grass(600, 350, 200) # ПЛАТФОРМА 2
 
 # создаю колобка
-kolobok = ph.Char(5, 5, '../res/graphics/kolobok01.png')
-kolobok = pg.transform.scale(kolobok, (50, 50))
+kolobok = ph.Char(5, 3, '../res/graphics/kolobok_50x50_right.png')
 kolobok.rect.x = 30
-kolobok.rect.y = level1.screen_height - 220
+kolobok.rect.y = 720 - 100 #level1.screen_height - 50
 all_sprites.add(kolobok)
 
 # ОСНОВНОЙ ЦИКЛ ИГРЫ
@@ -48,15 +58,37 @@ while running:
         if event.type == pg.QUIT: running = False
 
 
+# Обработка нажатий клавиш
+    keys = pg.key.get_pressed()
+    if keys[pg.K_a]:
+        kolobok.image = pg.image.load('../res/graphics/kolobok_50x50_left.png')
+        if kolobok.rect.left > 0: kolobok.rect.x -= kolobok.h_speed
+    if keys[pg.K_d]:
+        kolobok.image = pg.image.load('../res/graphics/kolobok_50x50_right.png')
+        if kolobok.rect.right < level1.screen_width: kolobok.rect.x += kolobok.h_speed
+    if keys[pg.K_SPACE]:
+        kolobok.jump()
 
+    kolobok.on_platform = kolobok.check_platforms_below(platforms)
+    if not kolobok.on_platform:
+        # Тут логика падения или "смерти" персонажа
+        print(kolobok.rect.x, kolobok.rect.y)
+        print("Персонаж должен падать или умереть")
+        #pg.quit()
+    else:
+        # Тут может быть логика, которая обрабатывает стояние игрока на платформе
+        print(kolobok.rect.x, kolobok.rect.y)
+        print("Персонаж находится на платформе")
 
 
 # Обновление спрайтов
     all_sprites.update()  # Важно вызвать метод update у всех спрайтов
+    platforms.update()
 
 # Рендеринг
-    screen.fill(resources.GREY)
+
     all_sprites.draw(screen)
+    platforms.draw(screen)
 
 # После отрисовки всего, переворачиваем экран
     pg.display.flip()
--- game/physics.py
+++ game/physics.py
@@ -1,6 +1,6 @@
 # Физический движок
 import pygame as pg
-import settings
+
 
 class Char(pg.sprite.Sprite):
     def __init__(self, health, speed, image):
@@ -12,24 +12,45 @@ class Char(pg.sprite.Sprite):
         self.image = pg.image.load(image)
         self.rect = self.image.get_rect()
         self.gravity = 0.5
-        self.jump_strength = -10
+        self.jump_strength = -11
 
     #определяем, есть под ним что-то, или надо упасть:
-    def on_ground(self):
-        # if self.rect.bottom >= HEIGHT - 30:
-        #     return True
-        # else:
-        #     return False
-        pass
+    def check_platforms_below(self, platforms):
+        # Сдвигаем rect игрока вниз на 1 пиксель для проверки столкновения с платформами
+        self.rect.y += 1
+        for platform in platforms:
+            # Проверяем столкновение игрока с платформой
+            if self.rect.colliderect(platform.rect):
+                # Если нашли столкновение с перемещением вниз,
+                # значит, под игроком есть платформа, и он не должен падать
+                self.rect.y -= 1  # Отменяем перемещение rect игрока
+                return True
 
-    # def update(self):
-    #     self.v_speed += self.gravity
-    #     self.rect.y += self.v_speed
-    #
-    #     if self.rect.bottom > pg.display.height - self.rect.height:
-    #         self.rect.bottom = pg.display.height - self.rect.height
-    #         self.on_ground = True
-    #         self.velocity = 0
+        # Если перебрали все платформы и не нашли столкновений,
+        # возвращаем игрока на его первоначальную позицию и сообщаем, что он должен падать
+        self.rect.y -= 1
+        return False
+
+    def jump(self):
+        if self.on_ground:
+            self.v_speed = self.jump_strength
+            self.on_ground = False
+
+# КОСТЫЛЬ!!!!!! надо потом сделать универсальную передачу констант, ведь уровни будут разные
+# сейчас проверяю только находждение на полу
+        if self.rect.bottom > 720-50: # тут надо понять, как поймать координату Х поверхности, над которой он находится
+            self.rect.bottom = 720-50
+            self.on_ground = True
+            self.v_speed = 0
+
+    def update(self):
+        self.v_speed += self.gravity
+        self.rect.y += self.v_speed
+
+        if self.rect.bottom > 720 - self.rect.height:
+            self.rect.bottom = 720 - self.rect.height
+            self.on_ground = True
+            self.velocity = 0
 
 '''
 # Обработка нажатий клавиш. Здесь именно факт зажатия и удерживания, если нужно непрерывное движение
