514ac7508238db27401e54b31bcbcabaf6798e75
ergwat <denis.anisimov@gmail.com>
2024-05-22 09:39:48 +0300

    создание уровней в level_manager

--- game/level_manager.py
+++ game/level_manager.py
@@ -1,38 +1,131 @@
 import pygame as pg
-from ui.components import Player, Obstacle, Enemy
 import game.resources as res
 
-def create_level(level_index):
-    background = pg.image.load(f"../res/graphics/fon_0{level_index}.jpg")
-    grass = pg.image.load(f"../res/graphics/grass_0{level_index}.png")
-    match level_index:
-        case 0:
-            pass
-        case 1:
-            pass
-        case 2:
-            pass
-        case 3:
-            pass
-        case 4:
-            pass
+class Level():
+    def __init__(self, index, platforms, enemies, obstacles, bonuses):
+        self.index = index
+        self.platforms_coord = platforms
+        self.enemies = enemies
+        self.obstacles = obstacles
+        self.bonuses = bonuses
+        self.bg = pg.sprite.Sprite()
+        self.bg_group = pg.sprite.Group() # отдельная группа спрайтов для одного фона
+        self.all_sprites = pg.sprite.Group()  # основные спрайты (колобок, враги, бонусы и т.п.)
+        self.platforms = pg.sprite.Group()  # платформы - отдельно для проверки коллизий (для прыжков и падений)
 
-def create_grass(x, y, length):  # вводные данные - стартовые координаты (х,у) и длина
-    platform_end = x + length
-    while x < platform_end:
-        grass = pg.sprite.Sprite()
-        grass.image = pg.image.load('../res/graphics/grass2.png')
-        grass.rect = grass.image.get_rect()
-        grass.rect.y = y - grass.rect.height  # Позиционируем у нижнего края окна
-        grass.rect.x = x
-        mainEngine.platforms.add(grass)
-        x += grass.rect.width  # Перемещаем X на ширину спрайта для следующего спрайта
-
-    # Создаем игрока
-    player = Player(level_index)  # Пример начальной позиции
+    def create_level(self):
+    # создание фона
+        self.bg.image = pg.image.load(f"../res/graphics/fon_0{self.index}.jpg")
+        self.bg.rect = self.bg.image.get_rect()
+        self.bg.rect.y = 0
+        self.bg.rect.x = 0
+        self.bg_group.add(self.bg)
+    # создание платформ
+        for platform in self.platforms_coord:
+            self.create_grass(platform[0], platform[1], platform[2])
 
 
+    def create_grass(self,x, y, length):  # вводные данные - стартовые координаты (х,у) и длина
 
+        platform_end = x + length
+        while x < platform_end:
+            grass = pg.sprite.Sprite()
+            grass.image = pg.image.load(f"../res/graphics/grass_0{self.index}.png")
+            grass.rect = grass.image.get_rect()
+            grass.rect.y = y - grass.rect.height  # Позиционируем у нижнего края окна
+            grass.rect.x = x
+            self.platforms.add(grass)
+            self.all_sprites.add(grass)
+            x += grass.rect.width  # Перемещаем X на ширину спрайта для следующего спрайта
+
+    def update(self):        # Обновление спрайтов
+        self.bg_group.update()
+        self.platforms.update()
+        self.all_sprites.update()
+        #print(self.all_sprites)
+
+    def draw(self, screen):
+        # Метод для отрисовки объектов уровня
+        self.bg_group.draw(screen)
+        self.platforms.draw(screen)
+        self.all_sprites.draw(screen)
+
+
+
+
+# Табло времени
+class TextBox(pg.sprite.Sprite):
+    def __init__(self, text, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.text = text
+        self.font = pg.font.Font("../res/font/arialbi.ttf", 30)
+        self.image = self.font.render(self.text, False, res.RED)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.centery = y
+        self.elapsed_time = 0
+        self.formatted_time = self.format_time(self.elapsed_time)
+
+    def format_time(self,milliseconds):
+        # Преобразуем миллисекунды в секунды
+        seconds = milliseconds // 1000
+        # Получаем минуты и секунды
+        minutes = seconds // 60
+        seconds = seconds % 60
+        # Форматируем время в строку MM:SS
+        time_str = f"{minutes:02}:{seconds:02}"
+        return time_str
+
+    def update(self):
+        self.text = self.format_time(self.elapsed_time)
+        self.image = self.font.render(self.text, False, res.RED)
+
+
+
+
+LEVEL1_PLATFORMS =[
+    [0, 720, 1200],
+    [200, 620, 300],
+    [500, 520, 200],
+    [350, 420, 100],
+    [500, 320, 400]
+]
+
+LEVEL1_ENEMIES = []
+LEVEL1_OBSTACLES = []
+LEVEL1_BONUSES = []
+
+
+
+
+LEVEL2_PLATFORMS =[
+    [0, 720, 1200],
+    [200, 620, 300],
+    [500, 520, 200],
+    [350, 420, 100],
+    [500, 320, 400]
+]
+
+LEVEL2_ENEMIES = []
+LEVEL2_OBSTACLES = []
+LEVEL2_BONUSES = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
     # Создаем врагов
     if level_index == 0:
         enemies = [
@@ -95,5 +188,5 @@ class LevelManager:
         for enemy in self.current_level["enemies"]:
             enemy.update()
             screen.blit(enemy.lisa, enemy.rect)
-
+'''
 
--- game/mainEngine.py
+++ game/mainEngine.py
@@ -1,92 +1,42 @@
 import pygame as pg
 import settings
-import resources
 import physics as ph
+import level_manager as lm
+
 
 # Переводит милисекунды в удобный формат
-def format_time(milliseconds):
-    # Преобразуем миллисекунды в секунды
-    seconds = milliseconds // 1000
-    # Получаем минуты и секунды
-    minutes = seconds // 60
-    seconds = seconds % 60
-    # Форматируем время в строку MM:SS
-    time_str = f"{minutes:02}:{seconds:02}"
-    return time_str
-
-# Текстовые блоки.
-class TextBox(pg.sprite.Sprite):
-    def __init__(self, text, x, y):
-        pg.sprite.Sprite.__init__(self)
-        self.text = text
-        self.font = pg.font.Font("../res/font/arialbi.ttf", 30)
-        self.image = self.font.render(self.text, False, resources.RED)
-        self.rect = self.image.get_rect()
-        self.rect.centerx = x
-        self.rect.centery = y
-
-    def update(self):
-        self.text = formatted_time
-        self.image = self.font.render(self.text, False, resources.RED)
 
 
-# функция для создания любых, но ОДИНАКОВЫХ платформ
-def create_grass(x, y, length): # вводные данные - стартовые координаты (х,у) и длина
-    platform_end = x+length
-    while x < platform_end:
-        grass = pg.sprite.Sprite()
-        grass.image = pg.image.load('../res/graphics/grass01.png')
-        grass.rect = grass.image.get_rect()
-        grass.rect.y = y - grass.rect.height  # Позиционируем у нижнего края окна
-        grass.rect.x = x
-        platforms.add(grass)
-        x += grass.rect.width  # Перемещаем X на ширину спрайта для следующего спрайта
 
-# пока задаём текщий уровень жёстко в коде, потом будет считываться из файла
-current_level=1
+
+
+# class Level (index, platforms, enemies, obstacles, bonuses)
+current_level = lm.Level(1, lm.LEVEL1_PLATFORMS, lm.LEVEL1_ENEMIES, lm.LEVEL1_OBSTACLES, lm.LEVEL1_BONUSES)
 
 pg.init()
+current_level.create_level()
 start_ticks = pg.time.get_ticks()
-level1 = settings.Settings()
-screen = pg.display.set_mode((level1.screen_width, level1.screen_height))
-pg.display.set_caption(level1.caption)
+settings = settings.Settings()
+screen = pg.display.set_mode((settings.screen_width, settings.screen_height))
+pg.display.set_caption(settings.caption)
 clock = pg.time.Clock()
 
-# Создаю 2 группы спрайтов (пока пустые), чтобы потом обновлять все разом
-all_sprites = pg.sprite.Group() # все остальные спрайты
-platforms = pg.sprite.Group() # платформы - отдельно для проверки коллизии (для прыжков и падений)
 
-# создаю фон
-bg = pg.sprite.Sprite()
-bg.image = pg.image.load('../res/graphics/fon_03-2.jpg')
-bg.rect = bg.image.get_rect()
-bg.rect.y = 0
-bg.rect.x = 0
-screen.blit(bg.image, (0, 0))
-#all_sprites.add(bg)
-
-# создаю землю и платформы
-create_grass(0, level1.screen_height,level1.screen_width) # ЗЕМЛЯ
-create_grass(200, 620, 300) # ПЛАТФОРМА 1
-create_grass(500, 520, 200) # ПЛАТФОРМА 2
-create_grass(350, 420, 100) # ПЛАТФОРМА 3
-create_grass(500, 320, 400) # ПЛАТФОРМА 4
 
 # создаю колобка
 kolobok = ph.Char(5, 3, '../res/graphics/kolobok_50x50_right.png', 670)
-kolobok.rect.x = 30
-kolobok.rect.y = level1.screen_height - 30
-all_sprites.add(kolobok)
+kolobok.rect = pg.Rect(30, settings.screen_height - 30, 50, 40)
+current_level.all_sprites.add(kolobok)
 
 # Создаю табло времени
-time_box = TextBox("00:00", 1100, 20)
-all_sprites.add(time_box)
+time_box = lm.TextBox("00:00", 1100, 20)
+current_level.all_sprites.add(time_box)
 
 # ОСНОВНОЙ ЦИКЛ ИГРЫ
 running = True
 while running:
     # Держим цикл на правильной скорости
-    clock.tick(level1.fps)
+    clock.tick(settings.fps)
     # Ввод процесса (события)
     for event in pg.event.get():
         if event.type == pg.QUIT: running = False
@@ -95,9 +45,9 @@ while running:
     # Получаем текущее время
     current_ticks = pg.time.get_ticks()
     # Вычисляем время работы приложения
-    elapsed_time = current_ticks - start_ticks
+    time_box.elapsed_time = current_ticks - start_ticks
     # Форматируем прошедшее время
-    formatted_time = format_time(elapsed_time)
+    time_box.formatted_time = time_box.format_time(time_box.elapsed_time)
 
 # Обработка нажатий клавиш
     keys = pg.key.get_pressed()
@@ -106,13 +56,13 @@ while running:
         if kolobok.rect.left > 0: kolobok.rect.x -= kolobok.h_speed
     if keys[pg.K_d]:
         kolobok.image = pg.image.load('../res/graphics/kolobok_50x50_right.png')
-        if kolobok.rect.right < level1.screen_width: kolobok.rect.x += kolobok.h_speed
+        if kolobok.rect.right < settings.screen_width: kolobok.rect.x += kolobok.h_speed
     if keys[pg.K_SPACE]:
         kolobok.jump()
 
 
-    if kolobok.find_nearest_platform(platforms):
-        kolobok.current_ground_y = kolobok.find_nearest_platform(platforms)[1]
+    if kolobok.find_nearest_platform(current_level.platforms):
+        kolobok.current_ground_y = kolobok.find_nearest_platform(current_level.platforms)[1]
 
 # Проверка столкновения
 #     if pg.sprite.spritecollide(kolobok, platforms, False):
@@ -121,14 +71,10 @@ while running:
 #         kolobok.on_ground = True
 
 
-# Обновление спрайтов
-    platforms.update()
-    all_sprites.update()
-
 # Рендеринг
-    screen.blit(bg.image, (0, 0))
-    platforms.draw(screen)
-    all_sprites.draw(screen)
+    current_level.update()
+    current_level.draw(screen)
+
 
 # После отрисовки всего, переворачиваем экран
     pg.display.flip()
